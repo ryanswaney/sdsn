8b45643a1b4033a5e95536ec86109f94662a6bcd
Ryan Swaney <ryan.swaney@gmail.com>
2017-08-09 15:22:11 -0400

    Updated GA code

--- template-parts/footers/tpl_footer_site.php
+++ template-parts/footers/tpl_footer_site.php
@@ -1,4 +1,4 @@
-			
+
       </div>
       <!-- inner wrap -->
     </div>
@@ -16,17 +16,22 @@
 
     <?php endif; ?>
 
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-34440011-1']);
-      _gaq.push(['_trackPageview']);
-                    
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();    
-    </script>
+		<!-- Google Analytics -->
+		<script>
+		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+		ga('create', 'UA-34440011-1', 'auto');
+		ga('send', 'pageview');
+		</script>
+		<script>
+		var trackOutboundLink = function(url) {
+		   ga('send', 'event', 'outbound', 'click', url, {
+		     'transport': 'beacon',
+		     'hitCallback': function(){document.location = url;}
+		   });
+		}
+		</script>
+		<script async src='https://www.google-analytics.com/analytics.js'></script>
+		<!-- End Google Analytics -->
 
   </body>
 </html>
